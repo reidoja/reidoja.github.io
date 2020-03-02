6a212d95e2d4774407c85b75b1e63893fbbfa0a8
reidoja <reiraja07@gmail.com>
2020-03-02 17:41:53 +0700

    otw skills

--- script.js
+++ script.js
@@ -11,36 +11,44 @@ var options = {
 
 var typed = new Typed('.typing', options);
 
-// var bar = new ProgressBar.Circle(container, {
-// 	color: '#aaa',
-// 	// This has to be the same size as the maximum width to
-// 	// prevent clipping
-// 	strokeWidth: 4,
-// 	trailWidth: 1,
-// 	easing: 'easeInOut',
-// 	duration: 1400,
-// 	text: {
-// 		autoStyleContainer: false
-// 	},
-// 	from: { color: '#aaa', width: 1 },
-// 	to: { color: '#333', width: 4 },
-// 	// Set default step function for all animate calls
-// 	step: function(state, circle) {
-// 		circle.path.setAttribute('stroke', state.color);
-// 		circle.path.setAttribute('stroke-width', state.width);
+function calculate_bar(text, percentage) {
+	var className = '.circle-' + text;
+	var bar = new ProgressBar.Circle(className, {
+		color: '#00ffff',
+		// This has to be the same size as the maximum width to
+		// prevent clipping
+		strokeWidth: 4,
+		duration: 1400,
+		text: {
+			autoStyleContainer: false
+		},
+		from: { color: '#00ffff', width: 1 },
+		to: { color: '#00ffff', width: 3 },
+		// Set default step function for all animate calls
+		step: function(state, circle) {
+			circle.path.setAttribute('stroke', state.color);
+			circle.path.setAttribute('stroke-width', state.width);
 
-// 		var value = Math.round(circle.value() * 100);
-// 		if (value === 0) {
-// 			circle.setText('');
-// 		} else {
-// 			circle.setText(value);
-// 		}
-// 	}
-// });
-// bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-// bar.text.style.fontSize = '2rem';
+			var value = Math.round(circle.value() * 100);
+			if (value === 0) {
+				circle.setText('');
+			} else {
+				circle.setText(value + '%');
+			}
+		}
+	});
+	bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+	bar.text.style.fontSize = '2rem';
 
-// bar.animate(0.8);
+	bar.animate(percentage);
+}
+
+calculate_bar('html', 0.8);
+calculate_bar('js', 0.75);
+calculate_bar('c', 0.85);
+calculate_bar('php', 0.75);
+calculate_bar('java', 0.85);
+calculate_bar('react', 0.7);
 
 var age = document.getElementById('age');
 
